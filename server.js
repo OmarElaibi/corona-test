39a5922194ba04ebdac5ded7fcf386ba1eece856
OmarElaibi <omar.elaibi@gmail.com>
2020-03-28 18:21:43 +0100

    auth for access try 3

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const auth = require('./auth')
+const basicAuth = require('express-basic-auth')
  
 const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/covitest'));
-
-app.use(auth);
  
-app.get('*', function(req,res) {
+app.get('*', basicAuth({
+    users: { 'youcode': 'youcode18' }
+}), function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname + '/dist/covitest/index.html'));
 });
