661a8abfca4650f5364766393f71dc5aeeb405ef
OmarElaibi <omar.elaibi@gmail.com>
2020-03-28 18:37:28 +0100

    auth for access try 4

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ const path = require('path');
 const basicAuth = require('express-basic-auth')
  
 const app = express();
+
+app.use(basicAuth({
+    users: { 'youcode': 'youcode18' },
+    challenge: true,
+    realm: 'Imb4T3st4pp',
+}))
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/covitest'));
+
+
  
-app.get('*', basicAuth({
-    users: { 'youcode': 'youcode18' }
-}), function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
+app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/covitest/index.html'));
 });
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
